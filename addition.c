b473278f61c5fbefb67ae479fc20926346acd651
KiranG <kiran@outlook.com>
2022-04-01 14:08:55 +0530

    fixed the QA bugs-commit-3

--- addition.c
+++ addition.c
@@ -1,10 +1,12 @@
+#include<stdio.h>
 main()
 
 {
 
 int a=5;
 int b=10;
+int c;
 
-printf("The value of addition is " &(a+b));
-
+printf("The value of addition is ", &c);
+getch();
 }
--- subtraction.c
+++ subtraction.c
@@ -1,10 +1,12 @@
+#include<stdio.h>
 main()
 
 {
 
 int a=5;
 int b=10;
+int c;
 
-printf("The value of subtraction is " &(a-b));
-
+printf("The value of subtraction is ", &c);
+getch();
 }
